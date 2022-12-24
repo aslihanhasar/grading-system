400a5d9f0ce4ab104f509d847eb53714bcb982ba
Aslıhan <aslihan_hasar@hotmail.com>
2022-12-24 20:38:13 +0300

    Add Teacher class codes via upload

---
+++ src/Teacher.java
@@ -0,0 +1,13 @@
+public class Teacher {
+    String name;
+    String surname;
+    String branch;
+    String match;
+
+    Teacher(String name,String surname,String branch,String match){
+        this.name=name;
+        this.surname=surname;
+        this.branch=branch;
+        this.match=match;
+    }
+}
